8b1822c1b2c49bd22f30c18e996e09487033dfba
mxslms <mkonwsh@gmail.com>
2025-03-19 20:45:28 -0400

    first run

--- merge_sort.py
+++ merge_sort.py
@@ -1,1 +1,39 @@
 #merge-sort.py
+def merge(arr1, arr2):
+    i = 0
+    j = 0
+    combolen = len(arr1) + len(arr2)
+    k = 0
+    arr3 = []
+    while k < combolen: 
+        if arr1[i] < arr2[j]:
+             arr3.append(arr1[i])
+             if i == len(arr1)-1:
+                 return arr3 + arr2[j:]
+             i = i+1
+        else:
+            arr3.append(arr2[j])
+            if j == len(arr2)-1:
+                return arr3 + arr1[i:]
+            j = j+1
+        k=k+1
+    return arr3
+
+def sort(a_list):
+    if len(a_list) <= 1:
+        return a_list
+    else:
+        half = len(a_list)//2
+        left = sort(a_list[:half])
+        right = sort(a_list[half:])
+        final = merge(left,right)
+        return final
+
+######
+#Main#
+######
+
+num = input('Enter a list of integers: ')
+#convert to list
+intlist = list(map(int, str(num)))
+print(sort(intlist))
